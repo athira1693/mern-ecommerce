d8653a208769a08932b264e9f71907a7c6c69ac9
Athira S <athira.s@hauniareo.onmicrosoft.com>
2023-10-28 23:18:13 +0530

    added frontend and backend

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import ProductCarouselComponent from '../components/ProductCarouselComponent'
+import CategoryCardComponent from '../components/CategoryCardComponent'
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+
+const HomePage = () => {
+  const categories = [
+    "Tablets",
+    "Monitors",
+    "Games",
+    "Printers",
+    "Software",
+    "Cameras",
+    "Books",
+    "Videos"
+  ]
+  return (
+    <>
+      <ProductCarouselComponent />
+      <Container style={{marginBottom:'130px'}}>
+        <Row xs={1} md={2} className="g-4 mt-5">
+          {
+            categories.map((category,idx) =>
+              <CategoryCardComponent key={idx} category={category} idx={idx}/>
+            )
+          }
+        </Row>
+      </Container>
+    </>
+  )
+}
+
+export default HomePage
